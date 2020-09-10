577f4bf9473eb6a0534c77e1993624d98ac8f9ff
Daniel TIllero <danieltillero67@gmail.com>
2020-09-10 18:11:55 -0400

    Checking for column win

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -1,9 +1,19 @@
 export default class ColumnWinInspector {
-	constructor(columnObj) {
-		this.columnObj = columnObj;
-	}
-	inspect() {
-    for(ç)
-
-  }
+    constructor(columnObj) {
+        this.columnObj = columnObj;
+    }
+    inspect() {
+        let columnArray = this.columnObj.columnArray;
+        for (let i = 0; i < 4; i++) {
+            if (
+                columnArray[i] &&
+                columnArray[i] === columnArray[i + 1] &&
+                columnArray[i + 1] === columnArray[i + 2] &&
+                columnArray[i + 2] === columnArray[i + 3] &&
+                columnArray[i + 3] === columnArray[i + 4]
+            ) {
+                return columnArray[i];
+            }
+        }
+    }
 }
--- game.js
+++ game.js
@@ -1,62 +1,74 @@
 import Column from "./column.js";
+import ColumnWinInspector from "./column-win-inspector.js";
 
 export default class Game {
-	constructor(player1Name, player2Name) {
-		this.player1Name = player1Name;
-		this.player2Name = player2Name;
-		this.currentPlayer = 1;
-		this.columns = [
-			new Column(0),
-			new Column(1),
-			new Column(2),
-			new Column(3),
-			new Column(4),
-			new Column(5),
-			new Column(6),
-		];
-		this.winnerNumber = 0;
-	}
+    constructor(player1Name, player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+        this.currentPlayer = 1;
+        this.columns = [
+            new Column(0),
+            new Column(1),
+            new Column(2),
+            new Column(3),
+            new Column(4),
+            new Column(5),
+            new Column(6),
+        ];
+        this.winnerNumber = 0;
+    }
 
-	getName() {
-		if (this.winnerNumber === 3) {
-			return `${this.player1Name} ties with ${this.player2Name}`;
-		} else {
-			return `${this.player1Name} vs. ${this.player2Name}`;
-		}
-	}
+    getName() {
+        if (this.winnerNumber === 3) {
+            return `${this.player1Name} ties with ${this.player2Name}`;
+        } else {
+            return `${this.player1Name} vs. ${this.player2Name}`;
+        }
+    }
 
-	playInColumn(columnIndex) {
-		this.columns[columnIndex].add(this.currentPlayer);
-		if (this.currentPlayer === 2) {
-			this.currentPlayer = 1;
-		} else {
-			this.currentPlayer = 2;
-		}
+    playInColumn(columnIndex) {
+        this.columns[columnIndex].add(this.currentPlayer);
+        if (this.currentPlayer === 2) {
+            this.currentPlayer = 1;
+        } else {
+            this.currentPlayer = 2;
+        }
 
-		this.checkForTie();
-	}
+        this.checkForTie();
+    }
 
-	checkForTie() {
-		//refactor with every() if time allows//
+    checkForTie() {
+        //refactor with every() if time allows//
 
-		let columnIndex = 0;
-		while (columnIndex <= 6) {
-			if (this.isColumnFull(columnIndex)) {
-				columnIndex++;
-			} else {
-				break;
-			}
-			if (columnIndex > 6) {
-				this.winnerNumber = 3;
-			}
-		}
-	}
+        let columnIndex = 0;
+        while (columnIndex <= 6) {
+            if (this.isColumnFull(columnIndex)) {
+                columnIndex++;
+            } else {
+                break;
+            }
+            if (columnIndex > 6) {
+                this.winnerNumber = 3;
+            }
+        }
+    }
 
-	getTokenAt(rowIndex, columnIndex) {
-		return this.columns[columnIndex].getTokenAt(rowIndex);
-	}
+    checkForColumnWin() {
+        if (this.winnerNumber !== 0) {
+            return;
+        } else {
+            for (let i = 0; i < this.columns.length; i++) {
+                let winInspector = new ColumnWinInspector(this.columns[i]);
+                winInsptor.inspect();
+            }
+        }
+    }
 
-	isColumnFull(columnIndex) {
-		return this.columns[columnIndex].isFull();
-	}
+    getTokenAt(rowIndex, columnIndex) {
+        return this.columns[columnIndex].getTokenAt(rowIndex);
+    }
+
+    isColumnFull(columnIndex) {
+        return this.columns[columnIndex].isFull();
+    }
 }
