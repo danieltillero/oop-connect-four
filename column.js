e4e58abf31c49e801da0441504666f0f27410552
julia-richards <julia.m.richards@gmail.com>
2020-09-10 14:20:28 -0600

    added tokens to board

--- column.js
+++ column.js
@@ -1,18 +1,19 @@
 export default class Column {
-    constructor(columnNumber) {
-        this.columnNumber = columnNumber;
-        this.columnArray = [null, null, null, null, null, null];
-    }
+	constructor(columnNumber) {
+		this.columnNumber = columnNumber;
+		this.columnArray = [null, null, null, null, null, null];
+	}
 
-    add(currentPlayer) {
-        for (let i = this.columnArray.length - 1; i > 0; i--) {
-            if (this.columnArray[i] === null) {
-                this.columnArray[i] = currentPlayer;
-            }
-        }
-    }
+	add(currentPlayer) {
+		for (let i = this.columnArray.length - 1; i > 0; i--) {
+			if (this.columnArray[i] === null) {
+				this.columnArray[i] = currentPlayer;
+				break;
+			}
+		}
+	}
 
-    getTokenAt(rowIndex) {
-        return this.columnArray[rowIndex];
-    }
+	getTokenAt(rowIndex) {
+		return this.columnArray[rowIndex];
+	}
 }
