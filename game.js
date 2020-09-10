424b025f1c3fdd3f76d77a8cff481a4caaffab69
julia-richards <julia.m.richards@gmail.com>
2020-09-10 16:32:57 -0600

    finished checking for column wins

--- game.js
+++ game.js
@@ -2,73 +2,84 @@ import Column from "./column.js";
 import ColumnWinInspector from "./column-win-inspector.js";
 
 export default class Game {
-    constructor(player1Name, player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-        this.currentPlayer = 1;
-        this.columns = [
-            new Column(0),
-            new Column(1),
-            new Column(2),
-            new Column(3),
-            new Column(4),
-            new Column(5),
-            new Column(6),
-        ];
-        this.winnerNumber = 0;
-    }
+	constructor(player1Name, player2Name) {
+		this.player1Name = player1Name;
+		this.player2Name = player2Name;
+		this.currentPlayer = 1;
+		this.columns = [
+			new Column(0),
+			new Column(1),
+			new Column(2),
+			new Column(3),
+			new Column(4),
+			new Column(5),
+			new Column(6),
+		];
+		this.winnerNumber = 0;
+	}
 
-    getName() {
-        if (this.winnerNumber === 3) {
-            return `${this.player1Name} ties with ${this.player2Name}`;
-        } else {
-            return `${this.player1Name} vs. ${this.player2Name}`;
-        }
-    }
+	getName() {
+		if (this.winnerNumber === 3) {
+			return `${this.player1Name} ties with ${this.player2Name}`;
+		} else if (this.winnerNumber === 1) {
+			return `${this.player1Name} wins!`;
+		} else if (this.winnerNumber === 2) {
+			return `${this.player2Name} wins!`;
+		} else {
+			return `${this.player1Name} vs. ${this.player2Name}`;
+		}
+	}
 
-    playInColumn(columnIndex) {
-        this.columns[columnIndex].add(this.currentPlayer);
-        if (this.currentPlayer === 2) {
-            this.currentPlayer = 1;
-        } else {
-            this.currentPlayer = 2;
-        }
+	playInColumn(columnIndex) {
+		this.columns[columnIndex].add(this.currentPlayer);
+		if (this.currentPlayer === 2) {
+			this.currentPlayer = 1;
+		} else {
+			this.currentPlayer = 2;
+		}
 
-        this.checkForTie();
-    }
+		this.checkForTie();
+		this.checkForColumnWin();
+	}
 
-    checkForTie() {
-        //refactor with every() if time allows//
+	checkForTie() {
+		//refactor with every() if time allows//
 
-        let columnIndex = 0;
-        while (columnIndex <= 6) {
-            if (this.isColumnFull(columnIndex)) {
-                columnIndex++;
-            } else {
-                break;
-            }
-            if (columnIndex > 6) {
-                this.winnerNumber = 3;
-            }
-        }
-    }
+		let columnIndex = 0;
+		while (columnIndex <= 6) {
+			if (this.isColumnFull(columnIndex)) {
+				columnIndex++;
+			} else {
+				break;
+			}
+			if (columnIndex > 6) {
+				this.winnerNumber = 3;
+			}
+		}
+	}
 
-    checkForColumnWin() {
-        if (this.winnerNumber !== 0) {
-            return;
-        } else {
-            for (let i = 0; i < this.columns.length; i++) {
-                let winInspector = new ColumnWinInspector(this.columns[i]);
-                winInsptor.inspect();
-            }
-        }
-    }
+	checkForColumnWin() {
+		if (this.winnerNumber !== 0) {
+			return;
+		} else {
+			for (let i = 0; i < this.columns.length; i++) {
+				let winInspector = new ColumnWinInspector(this.columns[i]);
+				let winner = winInspector.inspect();
+				if (winner === 1 || winner === 2) {
+					this.winnerNumber = winner;
+				}
+			}
+		}
+	}
 
-    getTokenAt(rowIndex, columnIndex) {
-        return this.columns[columnIndex].getTokenAt(rowIndex);
-    }
+	getTokenAt(rowIndex, columnIndex) {
+		return this.columns[columnIndex].getTokenAt(rowIndex);
+	}
 
-    isColumnFull(columnIndex) {
-        return this.columns[columnIndex].isFull();
-    }
+	isColumnFull(columnIndex) {
+		if (this.winnerNumber !== 0) {
+			return true;
+		}
+		return this.columns[columnIndex].isFull();
+	}
 }
