fddf6c077d843abc2032db5a0120045241900c44
Daniel TIllero <danieltillero67@gmail.com>
2020-09-10 13:52:27 -0400

    Added constructor and methods to game class

--- connect-four.js
+++ connect-four.js
@@ -1,42 +1,36 @@
-import { Game } from "./game.js";
+import Game from "./game.js";
 
 let game;
+const player1Input = document.getElementById("player-1-name");
+const player2Input = document.getElementById("player-2-name");
+const newGameBtn = document.getElementById("new-game");
 
-function updateUI() {
-	if (game) {
-		document.getElementById("board-holder").classList.add("is-invisible");
-	} else {
-		document
-			.getElementById("board-holder")
-			.classList.remove("is-invisible");
-	}
-}
+const disableGame = function () {
+    if (player1Input.value && player2Input.value) {
+        newGameBtn.disabled = false;
+    } else {
+        newGameBtn.disabled = true;
+    }
+};
 
-window.addEventListener("DOMContentLoaded", (e) => {
-	const player1Input = document.getElementById("player-1-name");
-	const player2Input = document.getElementById("player-2-name");
-	const newGameBtn = document.getElementById("new-game");
+const updateUI = function () {
+    if (game) {
+        document
+            .getElementById("board-holder")
+            .classList.remove("is-invisible");
+    } else {
+        document.getElementById("board-holder").classList.add("is-invisible");
+    }
+};
 
-	const disableGame = function () {
-		if (player1Input.value && player2Input.value) {
-			newGameBtn.disabled = false;
-		} else {
-			newGameBtn.disabled = true;
-		}
-	};
+player1Input.addEventListener("keyup", disableGame);
 
-	player1Input.addEventListener("keyup", (e) => {
-		disableGame();
-	});
+player2Input.addEventListener("keyup", disableGame);
 
-	player2Input.addEventListener("keyup", (e) => {
-		disableGame();
-	});
-
-	newGameBtn.addEventListener("click", (e) => {
-		let game = new Game();
-		player1Input.value = "";
-		player2Input.value = "";
-		disableGame();
-	});
+newGameBtn.addEventListener("click", (e) => {
+    game = new Game(player1Input.value, player2Input.value);
+    player1Input.value = "";
+    player2Input.value = "";
+    disableGame();
+    updateUI();
 });
--- game.js
+++ game.js
@@ -1,3 +1,19 @@
-class Game {
-  
+export default class Game {
+    constructor(player1Name, player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+        this.currentPlayer = 1;
+    }
+
+    getName() {
+        return `${this.player1Name} vs. ${this.player2Name}`;
+    }
+
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
+    }
 }
