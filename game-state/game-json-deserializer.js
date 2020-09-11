4aa53d51a2556bf3c0fb61b6d3cf5f249708730b
Daniel TIllero <danieltillero67@gmail.com>
2020-09-11 15:34:02 -0400

    Deserialize game state

--- game-state/game-json-deserializer.js
+++ game-state/game-json-deserializer.js
@@ -1,6 +1,15 @@
 export default class GameJsonDeserializer {
-	constructor(json) {
-		this.json = json;
-	}
-	deserialize() {}
+    constructor(json) {
+        this.json = json;
+    }
+    deserialize() {
+        let game = JSON.parse(this.json);
+        let currentIndexes = [5, 5, 5, 5, 5, 5, 5];
+        let instructions = [];
+        let valueToLookFor = 1;
+        for (let columnIndex = 0; columnIndex <= 6; columnIndex++) {
+            let rowIndex = currentIndexes[columnIndex];
+            let tokenValue = game[rowIndex][columnIndex];
+        }
+    }
 }
